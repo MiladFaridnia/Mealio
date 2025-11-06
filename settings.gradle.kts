9d72f19a2fe6662d4d17c9de53548c2c9a735ee7
Milad <m.faridnia@modam.ir>
2025-11-06 11:34:43 +0330

    Chore: V-3.1.1 Add mirror and JitPack repositories
    Adds two new Maven repositories to the project's dependency resolution settings. This ensures that the build system can fetch dependencies from an Iranian mirror and JitPack.
    
    Changes include:
    - Adds `https://en-mirror.ir` to `settings.gradle.kts`.
    - Adds `https://jitpack.io` to `settings.gradle.kts`.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,6 +16,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven("https://en-mirror.ir")
+        maven("https://jitpack.io")
     }
 }
 
